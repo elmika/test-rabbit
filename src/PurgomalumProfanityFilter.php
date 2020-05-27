73fda7215cea2de25b0fc8fd2703fbfc707638d8
mika <1506612+elmika@users.noreply.github.com>
2020-05-28 01:44:54 +0200

    Refactor: set up Purgomalum Service Provider.

---
+++ src/PurgomalumProfanityFilter.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace SquaredPoint;
+
+
+use GuzzleHttp\Client;
+
+class PurgomalumProfanityFilter
+{
+    private $guzzleClient;
+
+    public function __construct()
+    {
+        $this->guzzleClient = null;
+    }
+
+    private function createGuzzleClient() : void
+    {
+        $this->guzzleClient = new Client([
+            'base_uri' => 'https://www.purgomalum.com/service/',
+            'timeout' => 5,
+            'headers' => [
+                'Accept' => 'application/json'
+            ],
+        ]);
+    }
+
+    private function hasGuzzleClient()
+    {
+        return ! is_null($this->guzzleClient);
+    }
+
+    /**
+     * @param string $text
+     * @return string
+     * @throws InvalidJson
+     */
+    public function filter(string $text) : string
+    {
+        if( ! $this->hasGuzzleClient()){
+            $this->createGuzzleClient();
+        }
+
+        $response = $this->guzzleClient->get('json', ['query' => ['text' => $text]]);
+        $body = json_decode($response->getBody());
+        if(!$body){
+            throw new InvalidJson($response->getBody());
+        }
+        return $body->result;
+    }
+}
